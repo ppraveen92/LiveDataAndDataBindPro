cdf0320bd25b42949b9ef0ff0d6fdadaea30c310
ppraveen92 <ppraveen92@gmail.com>
2020-11-26 16:58:13 +0530

    Before Review

--- app/src/main/java/com/example/mvvmdatabinding/view/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmdatabinding/view/LoginActivity.kt
@@ -12,30 +12,23 @@ import com.example.mvvmdatabinding.databinding.ActivityLoginBinding
 import com.example.mvvmdatabinding.viewmodel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        val viewModel=ViewModelProvider(this).get(LoginViewModel::class.java)
-
-        binding.loginviewmodel=viewModel
-        binding.lifecycleOwner =this
-
-        viewModel.username.value=""
-        viewModel.pass.value=""
+        val viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.loginviewmodel = viewModel
+        binding.lifecycleOwner = this
+        viewModel.username.value = ""
+        viewModel.pass.value = ""
         viewModel.status.observe(this, Observer {
-            Toast.makeText(this,viewModel.status.value,Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, viewModel.status.value, Toast.LENGTH_SHORT).show()
         })
-
-
         viewModel.edittext.observe(this, Observer {
-            Toast.makeText(this,it.toString(),Toast.LENGTH_SHORT).show()
+          ShowToastMessage("Changing value "+viewModel.edittext.value)
         })
-
     }
 }
-
-fun Activity.ShowToastMessage(s:String){
-    Toast.makeText(this,s,Toast.LENGTH_SHORT).show()
+fun Activity.ShowToastMessage(s: String) {
+    Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
 }
